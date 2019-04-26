458585613d1d66a9851999b5d9b98e27e2fb9353
Oliver <8377772+oliver4888@users.noreply.github.com>
2019-04-26 21:59:46 +0100

    Add functions to loop through repos and fetch the languages for each. Add some logging to give the user feedback on what the script is doing. Fetch github api limits and then output to a json file.

--- index.js
+++ index.js
@@ -23,22 +23,43 @@ const options = url => {
   };
 };
 
-// request(
-//   options("https://api.github.com/rate_limit"),
-//   (error, _response, body) => {
-//     if (error) return console.log(error);
-//     outputs.limits = body;
-//   }
-// );
-
 request(
   options(`https://api.github.com/users/${GITHUB_USER}/repos`),
-  (error, response, body) => {
-    response.body = JSON.parse(response.body);
-    fs.writeFile(
-      "test-output.json",
-      JSON.stringify({ error: error, response: response }),
-      () => {}
-    );
+  (_error, _response, body) => {
+    fetchLanguages(JSON.parse(body), 0);
   }
 );
+
+function fetchLanguages(repos, idx) {
+  console.log(`Fetching languages for${repos[idx].private ? " private" : ""} repository: ${repos[idx].name}`);
+  request(options(repos[idx].languages_url), (_error, _response, body) => {
+    if (repos[idx].private) {
+      // add up languages
+    } else {
+      output.repos.push({
+        name: repos[idx].name,
+        languages: JSON.parse(body)
+      });
+    }
+
+    if (idx === repos.length - 1) {
+      fetchLimits();
+    } else {
+      fetchLanguages(repos, idx + 1);
+    }
+  });
+}
+
+function fetchLimits() {
+  request(
+    options("https://api.github.com/rate_limit"),
+    (_error, _response, body) => {
+      output.limits = JSON.parse(body);
+      outputToJson();
+    }
+  );
+}
+
+function outputToJson() {
+  fs.writeFile("output.json", JSON.stringify(output), () => {});
+}
